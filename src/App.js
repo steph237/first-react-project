9da5bf04a56e91b59a3b102948692ac85923bb73
steph237 <stephaniewnkwatoh@gmail.com>
2022-05-31 15:38:12 +0100

    Alpha movies

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Marvel");
   }, []);
 
   const searchMovies = async (title) => {
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>Alpha Movies</h1>
 
       <div className="search">
         <input
